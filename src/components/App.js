af3000ddaf7528bd886ad246e729114f09a90822
HueNguyen <nguyenhue.se@gmail.com>
2023-02-13 16:46:00 +1100

    done BrowseRequests

--- src/components/App.js
+++ src/components/App.js
@@ -1,174 +1,177 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import axios from 'axios'
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
 
-import SignUp from './SignUp';
-import Login from './Login';
+import SignUp from "./SignUp";
+import Login from "./Login";
 
-import MyRequests from '../pages/MyRequests';
-import MyOffers from '../pages/MyOffers';
-import BrowseRequests from '../pages/BrowseRequests';
-import Header from './Header';
-import Home from '../pages/Home';
-import NewRequest from '../pages/NewRequest';
+import MyRequests from "../pages/MyRequests";
+import MyOffers from "../pages/MyOffers";
+import BrowseRequests from "../pages/BrowseRequests";
+import Header from "./Header";
+import Home from "../pages/Home";
+import NewRequest from "../pages/NewRequest";
 
-const USERS_URL = 'http://localhost:3000/users.json'
+const USERS_URL = "http://localhost:3000/users.json";
 
 class App extends Component {
-
   state = {
     user: {},
     signinError: "",
-    signupError: ""
-  }
+    signupError: "",
+  };
 
-  componentDidMount(){
-    let token = localStorage.getItem('token')
-    if(token){
-      fetch('http://localhost:3000/profile', {
+  componentDidMount() {
+    let token = localStorage.getItem("token");
+    if (token) {
+      fetch("http://localhost:3000/profile", {
         method: "GET",
         headers: {
-          "Authorization": `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       })
-      .then(response => response.json())
-      .then(result => {
-        if(result.id){
-          this.setState({
-            user: result
-          })
-        }
-      })
+        .then((response) => response.json())
+        .then((result) => {
+          if (result.id) {
+            this.setState({
+              user: result,
+            });
+          }
+        });
     }
   }
 
-  signUp = user => {
-    fetch('http://localhost:3000/users', {
+  signUp = (user) => {
+    fetch("http://localhost:3000/users", {
       method: "POST",
       headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json"
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        user:{
+        user: {
           username: user.username,
           email: user.email,
           password: user.password,
-          password_confirmation: user.password_confirmation
-        }
-      })
-    })
-    .then(response => response.json())
-    .then(user => this.setState({ user: user }) )
-    .then(() =>{if (this.state.user.id == null) {
-      this.setState({
-        signupError: 'Invalid username, email or password',
-        signinError: '',
-        user: ''
-    })
-    } else {
-      this.setState({signupError: '', signinError: ''})
-      fetch("http://localhost:3000/login", {
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
+          password_confirmation: user.password_confirmation,
         },
-        body: JSON.stringify({
-            user: {
-                email: user.email
-            }
-        })
-      })
-      .then(response => response.json())
-      .then(result => {
-          if (result.token){
-          localStorage.setItem('token', result.token)
-          this.setState({
-              user: result.user
-              })
-          }
-      })
-
-
-
-
-
-    }
+      }),
     })
-
-  }
+      .then((response) => response.json())
+      .then((user) => this.setState({ user: user }))
+      .then(() => {
+        if (this.state.user.id == null) {
+          this.setState({
+            signupError: "Invalid username, email or password",
+            signinError: "",
+            user: "",
+          });
+        } else {
+          this.setState({ signupError: "", signinError: "" });
+          fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              user: {
+                email: user.email,
+              },
+            }),
+          })
+            .then((response) => response.json())
+            .then((result) => {
+              if (result.token) {
+                localStorage.setItem("token", result.token);
+                this.setState({
+                  user: result.user,
+                });
+              }
+            });
+        }
+      });
+  };
 
   signIn = (user) => {
     fetch("http://localhost:3000/login", {
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          email: user.email,
+          password: user.password,
         },
-        body: JSON.stringify({
-            user: {
-                email: user.email,
-                password: user.password
-            }
-        })
+      }),
     })
-    .then(response => response.json())
-    .then(result => {
-        if (result.token){
-        localStorage.setItem('token', result.token)
-        this.setState({
-            user: result.user
-            })
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.token) {
+          localStorage.setItem("token", result.token);
+          this.setState({
+            user: result.user,
+          });
+        } else {
+          this.setState({
+            signinError: result.error,
+            signupError: "",
+          });
         }
-        else {
-            this.setState({
-              signinError: result.error,
-              signupError: ''
-            })
-        }
-    })
-  }
+      });
+  };
 
   signOut = () => {
-    localStorage.removeItem('token')
+    localStorage.removeItem("token");
     this.setState({
-        user: null
-        })
-  }
-
+      user: null,
+    });
+  };
 
   render() {
     return (
       <div className="App">
-      <BrowserRouter>
-        <Header username = { this.state.user.username } />
-        <Routes>
-        <Route path="/signup" element={<SignUp  signupError={this.state.signupError}/>} />
-        <Route path="/login" element={<Login signIn={this.signIn} signinError={this.state.signinError}/>} />
-        <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/new-request' element={<NewRequest />} />
-        </Routes>
+        <BrowserRouter>
+          <Header username={this.state.user.username} />
+          <Routes>
+            <Route
+              path="/signup"
+              element={<SignUp signupError={this.state.signupError} />}
+            />
+            <Route
+              path="/login"
+              element={
+                <Login
+                  signIn={this.signIn}
+                  signinError={this.state.signinError}
+                />
+              }
+            />
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/new-request" element={<NewRequest />} />
+            <Route path="requests" element={<BrowseRequests />} />
+          </Routes>
+        </BrowserRouter>
 
+        {this.state.user.username ? (
+          <div>
+            <Logout onClick={this.signOut} />
+            <a
+              href={`/users/${this.state.user.username}`}
+            >{`(${this.state.user.username})`}</a>
+          </div>
+        ) : (
+          <a href="/login">login</a>
+        )}
 
-
-      </BrowserRouter>
-
-
-
-      {this.state.user.username ? <div><Logout onClick={this.signOut}/><a href={`/users/${this.state.user.username}`}>{`(${this.state.user.username})`}</a></div> : <a href='/login'>login</a>}
-
-
-
-
-
-
-
-        {this.state.user.username ? <h2>Welcome {this.state.user.username}</h2> : (
-          <>
-          </>)
-        }
+        {this.state.user.username ? (
+          <h2>Welcome {this.state.user.username}</h2>
+        ) : (
+          <></>
+        )}
       </div>
     );
   }
@@ -176,9 +179,10 @@ class App extends Component {
 
 export default App;
 
-
 const Logout = (props) => {
   return (
-    <a href="/logout" onClick={props.onClick}>Logout </a>
+    <a href="/logout" onClick={props.onClick}>
+      Logout{" "}
+    </a>
   );
 };
