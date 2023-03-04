e2a7fe1a551d587fdf970de08c00ad416c354557
loudringphone <loudringphone@gmail.com>
2023-03-05 00:16:44 +1100

    deploy

--- src/components/SERVER_URL.jsx
+++ src/components/SERVER_URL.jsx
@@ -1,3 +1,3 @@
-// export const SERVER_URL = 'https://threelancers-server.herokuapp.com/';
+export const SERVER_URL = 'https://threelancers-server.herokuapp.com/';
 
-export const SERVER_URL = 'http://localhost:3000/';
+// export const SERVER_URL = 'http://localhost:3000/';
